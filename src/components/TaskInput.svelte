58c29e783ed93f8a69a952748edf366b32f4d1fe
tannerkovach <kovach.tanner@gmail.com>
2022-01-17 09:13:53 -0800

    Removing autofocus from input for accessibility

--- src/components/TaskInput.svelte
+++ src/components/TaskInput.svelte
@@ -14,7 +14,7 @@
 <div class="min-w-[375px] max-w-[500px]">
 	<h1 class="text-4xl">Tasker</h1>
 	<form id="taskForm" on:submit|preventDefault={handleSubmit(taskTitle.value)} class="grid grid-cols-[1fr_auto] w-full justify-self-center mb-8">
-		<input id="taskTitle" type="text" placeholder="Add a new task" class="p-2 pl-4 rounded-md border-2 border-gray-300" bind:this={taskTitle} autofocus>
+		<input id="taskTitle" type="text" placeholder="Add a new task" class="p-2 pl-4 rounded-md border-2 border-gray-300" bind:this={taskTitle}>
 		<button type="submit" class="bg-sky-700 shadow-lg shadow-sky-700/50 p-2 rounded-full ml-2 text-gray-50">
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
